029fe124ee88baab1d025cc6f76d3d407c9e35f4
Laxshan19 <laxshan1906@gmail.com>
2025-02-28 23:48:36 +0530

    complete the insert part in crud

--- server/app.js
+++ server/app.js
@@ -21,6 +21,22 @@ const db=mysql.createConnection({
     port:3307
 })
 
+app.post('/add_user',(req,res)=>{
+    const sql = "INSERT INTO employees (`name`, `email`, `age`, `gender`) VALUES (?,?,?,?)";
+    const values=[
+        req.body.name,
+        req.body.email,
+        req.body.age,
+        req.body.gender
+    ]
+    db.query(sql,values,(err,result)=>{
+        if(err) return res.json({message:'Something unexpexted has occured'+err})
+        return res.json({success:"Employee added Successfully!"})
+    })
+      
+    
+})
+
 db.connect((err)=>{
     if(err){
     console.log("Database connection failed: " + err.stack);
